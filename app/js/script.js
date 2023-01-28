9e2fcd604f54a5190245e3b660531d04c0e9b922
Klostur <david.divijakov@gmail.com>
2023-01-28 16:49:44 +0100

    responsive navbar and filter script

--- app/js/script.js
+++ app/js/script.js
@@ -10,3 +10,23 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('scrolled');
     }
 })
+
+function filterFeatures() {
+    let select = document.getElementById("filter");
+
+    let selectedCategory = select.value;
+    // If a default "Select category" option is selected, the value become "none" (facepalm)
+    console.log(selectedCategory.value);
+    let featureItems = document.getElementsByClassName("features__item");
+
+    for (let i = 0; i < featureItems.length; i++) {
+
+      let featureCategory = featureItems[i].getAttribute("data-category");
+
+      if (selectedCategory === "none"  || selectedCategory === featureCategory) {
+        featureItems[i].style.display = "flex";
+      } else {
+        featureItems[i].style.display = "none";
+      }
+    }
+  }
